38c28b3345b2b6f35650d7055d8e443ac9560fbc
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-08-02 16:18:20 -0400

    Add files via upload

--- pcaAllDweck.R
+++ pcaAllDweck.R
@@ -6,6 +6,6 @@ library(stats)
 dweckStudies1_2_4.orig <- read.csv("dweckAllData.csv")
 dweckStudies1_2_4.itemsOnly <- dweckStudies1_2_4.orig[,3:ncol(dweckStudies1_2_4.orig)]
 nfactors(dweckStudies1_2_4.itemsOnly)  ## hmmm...
-dweckStudies1_2_4.pca <- principal(dweckStudies1_2_4.itemsOnly,nfactors=2,rotate="varimax")
+dweckStudies1_2_4.pca <- principal(dweckStudies1_2_4.itemsOnly,nfactors=3,rotate="cluster")
 print(dweckStudies1_2_4.pca)
 write.csv(dweckStudies1_2_4.pca$loadings,"studies1_2_4Loadings.csv")
